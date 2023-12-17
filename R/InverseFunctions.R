8746465afb70ab89fcebfa1ca95f9218424ede5d
Hannah Kümpel <hannahkumpel@Hannahs-MacBook-Pro.local>
2023-12-17 13:12:00 +0100

    Inverse Function data

---
+++ R/InverseFunctions.R
@@ -0,0 +1,23 @@
+id<-function(x){x}
+
+torch_plogis<-function(x){1/(1+torch_exp(-x))}
+
+torch_inv_probit<-function (eta) {
+  thresh <- -qnorm(.Machine$double.eps)
+  eta <- torch_minimum(torch_maximum(eta, -thresh), thresh)
+  0.5*(1+torch_erf(eta/sqrt(2)))}
+
+Inverse.Functions<-data.frame(Link=c("logit",
+                             "logistf",
+                             "log",
+                             "identity",
+                             "probit"),
+                      Inverse=c("torch_plogis",
+                                "torch_plogis",
+                                "torch_exp",
+                                "id",
+                                "torch_inv_probit")
+                      )
+
+usethis::use_data(Inverse.Functions,internal=TRUE)
+
