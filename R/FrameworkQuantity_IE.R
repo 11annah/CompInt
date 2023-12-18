fa048abbcacbe3a6dcb3491e97e8ede0b61b967d
Hannah Kümpel <hannahkumpel@Hannahs-MacBook-Pro.local>
2023-12-18 21:16:44 +0100

    .

--- R/FrameworkQuantity_IE.R
+++
@@ -1,6 +0,0 @@
-#' @export
-get_gME<-function(model_fit,assumption=NULL,reg_of_interest=NULL,seed=NULL,ndraws=1000,integration="empirical",separate_interactions=FALSE,newdata=NULL,subset=NULL,catRIbin=FALSE,...){
-  eval(ChunkList$getting_situated)
-
-
-}
