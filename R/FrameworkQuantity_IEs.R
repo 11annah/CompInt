5b17c553f201c6b626b63eb81210a6af1b3fc36d
Hannah Kümpel <hannahkumpel@Hannahs-MacBook-Pro.local>
2023-12-19 09:34:21 +0100

    name change

--- R/FrameworkQuantity_IEs.R
+++ R/FrameworkQuantity_IEs.R
@@ -1,5 +1,5 @@
 #' @export
-get_gME<-function(model_fit,assumption=NULL,reg_of_interest=NULL,seed=NULL,ndraws=1000,integration="empirical",separate_interactions=FALSE,newdata=NULL,subset=NULL,catRIbin=FALSE,...){
+get_IE<-function(model_fit,assumption=NULL,reg_of_interest=NULL,seed=NULL,ndraws=1000,integration="empirical",separate_interactions=FALSE,newdata=NULL,subset=NULL,catRIbin=FALSE,...){
   eval(ChunkList$getting_situated)
 
 
