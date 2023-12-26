792a0bda74ff79fea1c8556a112381682cc5836c
Hannah Kümpel <hannahkumpel@Hannahs-MBP.fritz.box>
2023-12-26 23:56:49 +0100

    torch option

--- R/InverseFunctions.R
+++ R/InverseFunctions.R
@@ -1,11 +1,11 @@
-#id<-function(x){x}
+id<-function(x){x}
 
-#torch_plogis<-function(x){1/(1+torch_exp(-x))}
+torch_plogis<-function(x){1/(1+torch_exp(-x))}
 
-#torch_inv_probit<-function (eta) {
-#  thresh <- -qnorm(.Machine$double.eps)
-#  eta <- torch_minimum(torch_maximum(eta, -thresh), thresh)
-#  0.5*(1+torch_erf(eta/sqrt(2)))}
+torch_inv_probit<-function (eta) {
+  thresh <- -qnorm(.Machine$double.eps)
+  eta <- torch_minimum(torch_maximum(eta, -thresh), thresh)
+  0.5*(1+torch_erf(eta/sqrt(2)))}
 
 
 
