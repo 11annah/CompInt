26204bd670e6ed8ad1c72dd91a2664d8818081da
Hannah Kümpel <hannahkumpel@Hannahs-MacBook-Pro.local>
2024-01-11 13:04:27 +0100

    update

--- inst/python_scripts/ProbInt_LinPred.py
+++ inst/python_scripts/ProbInt_LinPred.py
@@ -1,6 +1,10 @@
 from functools import reduce
 import operator
 import torch
+from torchquad import MonteCarlo, set_up_backend
+set_up_backend("torch", data_type="float32")
+mc = MonteCarlo()
+
 with open('inst/python_scripts/InverseFunctions.py', 'r') as f:
     script_code = f.read()
 
@@ -19,8 +23,23 @@ def simplegrad(ints,data,LinPred,thetas,fun=None,grad_variable=None):
   domains=list(ints.values())
   
   def function(x):
+    x_values = [x[:, i].repeat(len(list(data.values())[0]), 1) for i in range(x.size(1))]
     x_dict = dict(zip(ints.keys(), x_values))
+    globals().update(x_dict) 
     
+    if inv_link_fun is not None:
+        LinPred_val = eval(f'inv_link_fun({LinPred})')
+    else:
+        LinPred_val = eval(LinPred)
+        
+    return(LinPred_val.mean(dim=0))
     
-    
+  final_result = mc.integrate(
+                              function,
+                              dim=len(domains),
+                              N=10000,
+                              integration_domain=domains,
+                              backend="torch",
+                              )
+  return(final_result)
   
