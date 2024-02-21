a788d9b02a3ac7784cbd2e33f55da7f5c386b9ac
Hannah Schulz-Kümpel <80972004+11annah@users.noreply.github.com>
2024-02-21 18:24:38 +0100

    check pymods

---
+++ R/python_modules.R
@@ -0,0 +1,14 @@
+#' @export
+#'
+check_py_modules <- function(pythonhome=reticulate::py_config()$pythonhome){
+message("Checking for the required python modules...")
+required_modules <- c("torch","torchquad")
+missing_modules <- required_modules[which(!required_modules %in% reticulate::py_list_packages()$package)]
+if(length(missing_modules != 0)){
+  print(paste0("The python module(s) ",paste(missing_modules,collapse=" "),"
+                 are not installed in the specified pythonhome",pythonhome,
+               ".\n "))
+}else{
+    print("All required python modules are present!")
+  }
+}
--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
+torch <- NULL
+torchquad <- NULL
+
 .onLoad <- function(libname, pkgname) {
-  reticulate::configure_environment(pkgname)
+  # use superassignment to update global reference to scipy
+  torch <<- reticulate::import("torch", delay_load = TRUE)
+  torchquad <<- reticulate::import("torchquad", delay_load = TRUE)
+  check_py_modules()
 }
