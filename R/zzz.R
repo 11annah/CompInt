a3af2f1c36600ba2fdb71605ad3c53fb8baedfd0
Hannah Schulz-Kümpel <80972004+11annah@users.noreply.github.com>
2024-02-22 09:47:55 +0100

    weird

--- R/zzz.R
+++ R/zzz.R
@@ -2,15 +2,15 @@ torch <- NULL
 torchquad <- NULL
 
 .onLoad <- function(libname, pkgname) {
-  invisible(reticulate::py_config())
-  if(!reticulate::py_available()){
-    cat("Calling reticulate::py_available() returns 'FALSE'. Unless this is remidied and the required python modules are installed, you will not be able to access the full functionality of the CompInt package.",fill=TRUE)
-  }
-
   reticulate::use_virtualenv("r-CompInt", required = FALSE)
   # use superassignment to update global reference to scipy
   torch <<- reticulate::import("torch", delay_load = TRUE)
   torchquad <<- reticulate::import("torchquad", delay_load = TRUE)
+
+  invisible(reticulate::py_config())
+  if(!reticulate::py_available()){
+    cat("Calling reticulate::py_available() returns 'FALSE'. Unless this is remidied and the required python modules are installed, you will not be able to access the full functionality of the CompInt package.",fill=TRUE)
+  }
 
   check_py_modules()
 }
