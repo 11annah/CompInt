4270a1bed8a8f0680dde1da75a669f35078b1d36
Hannah Schulz-Kümpel <80972004+11annah@users.noreply.github.com>
2024-02-22 09:40:57 +0100

    .

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,7 @@ torch <- NULL
 torchquad <- NULL
 
 .onLoad <- function(libname, pkgname) {
+  invisible(reticulate::py_config())
   if(!reticulate::py_available()){
     cat("Calling reticulate::py_available() returns 'FALSE'. Unless this is remidied and the required python modules are installed, you will not be able to access the full functionality of the CompInt package.",fill=TRUE)
   }
