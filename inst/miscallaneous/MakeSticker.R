db11bd3033c9f762aed016347af8bcad87007898
Hannah Schulz-Kümpel <80972004+11annah@users.noreply.github.com>
2024-03-08 22:00:37 +0100

    logo

--- inst/miscallaneous/MakeSticker.R
+++ inst/miscallaneous/MakeSticker.R
@@ -1,9 +1,10 @@
 #library(hexSticker)
 imgurl <- system.file("figures/world.png", package="CompInt")
 s<-sticker(imgurl, package="CompInt",
-           p_size=75, p_y=0.5, p_family = "wqy-microhei", p_color = "#1C75BC", ##27AAE1
+           p_size=75, p_y=0.5, p_x=0.98 , p_family = "wqy-microhei", p_color = "#1C75BC", ##27AAE1
            s_x=1, s_y=1.15,
            h_fill="#EFFEFF",h_color = "#1C75BC",
            s_width=.6,
-           dpi=1000)
+           dpi=1000,
+           filename = paste0(package, "man/figures/logo.png"))
 plot(s)
