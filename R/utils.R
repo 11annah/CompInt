0805c32d3d9603a7631a2ca5fa0822e04afe7ed2
Hannah Schulz-Kümpel <80972004+11annah@users.noreply.github.com>
2024-03-11 10:00:25 +0100

    .

--- R/utils.R
+++ R/utils.R
@@ -44,25 +44,6 @@ data_prep <- function(mod, data = NULL, separate_interactions = FALSE) {
   dat <- data[, regs(mod), drop = FALSE]
   dat <- dat[complete.cases(dat), , drop = FALSE]
 
-  if (separate_interactions) {
-    if (!mod[["model_specification"]][["regs"]][["interactions"]][["present"]]) {
-      stop("'seperate_interactions' is specified as TRUE, but are no interaction terms present in the model.")
-    }
-
-    terms <- mod[["model_specification"]][["regs"]][["interactions"]][["terms"]]
-    notation <- mod[["model_specification"]][["regs"]][["interactions"]][["notation"]]
-
-    INTs <- data.frame(
-      terms = stringr::str_replace(terms, paste0("\\", notation), "_x_"),
-      expr = stringr::str_replace(terms, paste0("\\", notation), "*")
-    )
-
-    for (i in seq_along(INTs$terms)) {
-      dat[[INTs$terms[i]]] <- with(dat, {
-        eval(parse(text = INTs$expr[i]))
-      })
-    }
-  }
   return(dat)
 }
 
