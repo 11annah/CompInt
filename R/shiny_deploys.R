079bf974abd4984c61013d4b7c77a53ffae0376b
Hannah Schulz-Kümpel <80972004+11annah@users.noreply.github.com>
2024-04-20 16:53:44 +0200

    shiny app update

--- R/shiny_deploys.R
+++ R/shiny_deploys.R
@@ -1,8 +1,9 @@
 #' @export
 
 find_my_method <- function(){
-  httpuv::runStaticServer(system.file("shiny","decisiontree_app","shinydocs",package="CompInt"))
-}
+  #httpuv::runStaticServer(system.file("shiny","decisiontree_app","shinydocs",package="CompInt"))
+  shiny::runApp(system.file("shiny","decisiontree_app",package="CompInt"))
+  }
 
 
 
