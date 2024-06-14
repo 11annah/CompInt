d4d6151f20cafef7c70e77a51bea1285f1b51c27
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-06-14 13:59:20 +0200

    fix: add missing stats imports

---
+++ R/CompInt-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats coef
+#' @importFrom stats complete.cases
+#' @importFrom stats df
+#' @importFrom stats family
+#' @importFrom stats model.matrix
+#' @importFrom stats pnorm
+#' @importFrom stats qnorm
+#' @importFrom stats terms
+#' @importFrom stats vcov
+## usethis namespace: end
+NULL
