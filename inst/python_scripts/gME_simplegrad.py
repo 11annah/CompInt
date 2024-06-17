6afecd627173f3f14fe9bc7fcd072a8b77c6c27d
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-06-17 11:56:20 +0200

    refactor: more python type hints

--- inst/python_scripts/gME_simplegrad.py
+++ inst/python_scripts/gME_simplegrad.py
@@ -6,10 +6,10 @@ from InverseFunctions import make_inv_link_function
 
 
 def simplegrad(
-    data,
-    LinPred,
-    thetas,
-    grad_variable,
+    data: dict[str, list],
+    LinPred: str,
+    thetas: list[float],
+    grad_variable: str,
     fun: Optional[str] = None,
 ):
     inv_link_fun = make_inv_link_function(fun) if fun is not None else None
@@ -18,9 +18,10 @@ def simplegrad(
         key: torch.tensor(value, dtype=torch.float64, requires_grad=True)
         for key, value in data.items()
     }
+
     globals().update(tensor_dict)
 
-    def grad_for_theta(theta):
+    def grad_for_theta(theta: torch.Tensor) -> float:
         if inv_link_fun is not None:
             LinPred_val = eval(f"inv_link_fun({LinPred})")
         else:
@@ -29,13 +30,10 @@ def simplegrad(
         LinPred_val.backward(torch.ones_like(LinPred_val), retain_graph=True)
 
         gradient = eval(f"{grad_variable}.grad")
-
         result = torch.mean(gradient)
 
         return result.item()
 
-    # final_res = [grad_for_theta(theta=torch.tensor(entry, dtype=torch.double),LinPred=LinPred,grad_variable=grad_variable,inv_link_fun=inv_link_fun) for entry in thetas]
-
-    final_res = grad_for_theta(theta=torch.tensor(thetas, dtype=torch.double))
-
+    theta_tensor = torch.tensor(thetas, dtype=torch.double)
+    final_res = grad_for_theta(theta_tensor)
     return final_res
